fd00bb4cc0ee5adfafabaa1683a0cc65d57880f5
arielbob <arielbdilla@gmail.com>
2020-02-28 13:07:22 -0500

    Fix create transaction tests

--- src/transaction/create_trans.cpp
+++ src/transaction/create_trans.cpp
@@ -14,42 +14,46 @@ bool CreateTransaction::verify(User* user) {
 
     if (user->user_type == "AA") return true;
 
+    cout << "Error: Transaction not permitted for your user type" << endl;
     return false;
 }
 
 int CreateTransaction::execute(TransactionFile tf, User* current_user, map<string, User*> users) {
-    if (!verify(current_user)) {
-        cout <<"Error: prohibit to create account" << endl;
-        return 1;
-    }
+    if (!verify(current_user)) return 1;
 
     string username;
     string user_type;
 
     cout << "Please enter the username:" << endl;
     getline(cin, username);
-    if(username.size() > 15 || username.size() <= 0){
-        cout <<"Error: username too long or too short" <<endl;
+
+    if (username.size() > 15) {
+        cout << "Error: Username too long" << endl;
+        return 1;
+    }
+
+    if (username.size() == 0){
+        cout << "Error: Username too short" << endl;
         return 1;
     }
 
     map<string, User*>::iterator it = users.find(username);
     if(it == users.end()){
     } else{
-        cout <<"Error: username Already been take \n";
+        cout <<"Error: Username already taken" << endl;
         return 2;
     }
 
     //end of testing username
-    cout << "Please enter the user_type:" << endl;
+    cout << "Please enter the user type:" << endl;
     getline(cin, user_type);
     if(user_type != "AA" && user_type != "FS" && user_type != "SS" && user_type != "BS"){
-        cout <<"Error: user type is invaild" <<endl;
+        cout <<"Error: Invalid user type" <<endl;
         return 3;
     }
     //end of testing user_type
 
-    cout <<"Account Create Successful! \n";
+    cout << "Account created!" << endl;
 
     //
     // TODO
