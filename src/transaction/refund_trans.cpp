aca1fc2754d23bf15829f2f9304249448e79182e
arielbob <arielbdilla@gmail.com>
2020-02-28 14:24:53 -0500

    Fix tests for refund 1-7

--- src/transaction/refund_trans.cpp
+++ src/transaction/refund_trans.cpp
@@ -12,7 +12,7 @@ bool RefundTransaction::verify(User* user) {
     }
 
     if(user->user_type != "AA"){
-        cout <<"Error: Transaction not permitted for your user type" << endl;
+        cout << "Error: Transaction not permitted for your user type" << endl;
         return false;
     }
 
@@ -23,48 +23,48 @@ int RefundTransaction::execute(TransactionFile tf, User* current_user, map<strin
     if (!verify(current_user)) return 1;
 
     string buyer;
-    string seller;
-    string input_amount;
-
-    cout << "Please enter the buyer's name:" << endl;
+    cout << "Please enter the buyer's username:" << endl;
     getline(cin, buyer);
     map<string, User*>::iterator buyerit = users.find(buyer);
     if(buyerit == users.end()){
-        cout <<"Error: Username does not exist! \n";
+        cout << "Error: Username does not exist" << endl;
         return 2;
     } else if(buyerit ->second -> user_type == "SS"){
-        cout <<"Error: User is not of type buy-standard. \n";
+        cout <<"Error: User is not of type buy-standard" << endl;
         return 1;
     }
+
+    string seller;
     cout << "Please enter the seller's username:" << endl;
     getline(cin, seller);
     map<string, User*>::iterator sellerit = users.find(seller);
     if(sellerit == users.end()){
-        cout <<"Error: Username does not exist! \n";
+        cout << "Error: Username does not exist" << endl;
         return 2;
-    }else if(sellerit -> second -> user_type == "SS"){
-        cout <<"Error: User is not of type sell-standard.\n";
+    }else if(sellerit->second->user_type == "BS"){
+        cout << "Error: User is not of type sell-standard" << endl;
         return 1;
     }
+
+    string input_amount;
     cout << "Please enter the amount to refund:" << endl;
     getline(cin, input_amount);
     double amount = atof(input_amount.c_str());
 
-
-    if (amount < 0){
-        cout <<"Error: Amount to refund must be greater than zero!";
+    if (amount <= 0){
+        cout << "Error: Amount to refund must be greater than zero" << endl;
         return 3;
-    } else if((sellerit -> second ->credit + amount) >999999.99){
-        cout <<"Error: Resulting funds exceed limit in seller's account. \n";
+    } else if((buyerit->second->credit + amount) > 999999.99){
+        cout << "Error: Resulting funds exceed limit in buyer's account" << endl;
         return 4;
     } else if(sellerit -> second ->credit < amount){
-        cout <<"Error: Seller has insufficient funds for refund. \n";
+        cout << "Error: Seller has insufficient funds for refund" << endl;
         return 5;
     }
     //
     //TODO refund
     //
 
-    cout <<"Refund Successful! \n";
+    cout << "Refund successful!" << endl;
     return 0;
 }
